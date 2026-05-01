35e74cab9d5ef98f25ed50c1b7a39b50b59a482f
japaafk <matheushirose4@gmail.com>
2026-05-01 20:11:16 -0300

    estrutura de dicionário

--- japa/estruturas de dados/dict.py
+++ japa/estruturas de dados/dict.py
@@ -4,16 +4,24 @@ weight = float(input('Seu peso: '))
 
 # dicionários idetificam elementos por nomes e não por números como as listas
 pessoas = {
-            'nome': user_id,
+            'nome': user_id, # chave: valor
             'idade': age,
             'peso': weight,
            }
 
+print('\n')
 print(pessoas.keys()) # vai me dar a identificação do elemento
+print('\n')
 print(pessoas.values()) # vai me dar o valor
+print('\n')
 print(pessoas.items()) # vai me dar tanto a identificação quanto o valor
+print('\n')
+print(pessoas)
+print('\n')
 print(pessoas.get('idade', 'a variável idade não existe')) # a vantagem dessa forma é que você pode definir um valor padrão, assim não haverá erro se a chave não existir.
-print(f'Você se chama {pessoas["nome"]}, possui {pessoas["idade"]} anos e pesa {pessoas["peso"]:.1f}kg')
+print('\n')
+print(pessoas.update({'nome': 'Fulaninho', 'altura': 1.8})) # este método atualiza os pares chave-valor com os pares chave-valor de outro dicionário. Se eles tiverem chaves em comum, seus valores serão sobrescritos.
+print('\n')
+print(pessoas)
 
-# for chave, valor in dados.items():
-# print(chave, valor)
+# pessoas.clear() - método para remoção de todos chave-valor do dicionário
