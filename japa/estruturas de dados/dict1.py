f9c0ad856c205f056591622bafe6675d7939b69c
japaafk <matheushirose4@gmail.com>
2026-05-13 23:58:39 -0300

    dicionários dentro de lista

--- japa/estruturas de dados/dict1.py
+++ japa/estruturas de dados/dict1.py
@@ -1,8 +1,8 @@
 # APLICANDO UM DICIONÁRIO DENTRO DE UMA LISTA, PERMITINDO UMA ORGANIZAÇÃO MAIS DINÂMICA DE DADOS
 pessoas = [
-    {'nome': 'Matheus', 'numero': '1899332085'},
-    {'nome': 'Tevo', 'numero': '17993357789'},
-    {'nome': 'Leo', 'numero': '18990045678'}
+    {'nome': 'Matheus', 'numero': '1899332085', 'email': 'nadaave2026@gmail.com'}, # dict 1
+    {'nome': 'Tevo', 'numero': '17993357789'}, # dict 2
+    {'nome': 'Leo', 'numero': '18990045678'} # dict 3
 ]
 
 nome = input('Nome: ')
