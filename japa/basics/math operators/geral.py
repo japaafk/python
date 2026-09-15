a35828b024aaf09e7c50f0ee7a5e0936167b3d9e
japaafk <matheushirose4@gmail.com>
2026-09-15 12:48:07 -0300

    adiciona operador de divisão exata

--- japa/basics/math operators/geral.py
+++ japa/basics/math operators/geral.py
@@ -6,6 +6,7 @@ print(f'a multiplicação deles é {numero1*numero2}')
 print(f'a divisão deles é {numero1/numero2}')
 print(f'o resto da divisão deles é {numero1%numero2}')
 print(f'a potência desses números é {numero1**numero2}')
+print(f'a divisão exata deles é {numero1//numero2}')
 
 # um número é par quando dividimos por 2 e o resto for zero
 # se x % 2 == 0 é par
